89aa0d9093bc8a7700576ee321caa4eb72280e12
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-19 12:35:57 +1000

    Remove conda.

--- auto-docker-deployment-family/docker-image-client/docker-entrypoint.sh
+++ auto-docker-deployment-family/docker-image-client/docker-entrypoint.sh
@@ -18,8 +18,5 @@ kill -SIGHUP $(pidof dockerd)
 # Wait for Docker to reload its configuration
 sleep 5
 
-# Activate conda environment
-source /opt/conda/bin/activate myenv
-
 # Execute the main command
 exec "$@"
