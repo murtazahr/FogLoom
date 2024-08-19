49247cf68ade2237cb190120ccdb688155d51d4c
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-19 13:51:07 +1000

    Fix "Unable to access docker-entrypoint.sh outside build context".

---
+++ auto-docker-deployment-family/docker-image-event-handler/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+# Start the Docker daemon
+dockerd &
+
+# Wait for the Docker daemon to start
+while ! docker info >/dev/null 2>&1; do
+    echo "Waiting for Docker daemon to start..."
+    sleep 1
+done
+
+# Add the registry as an insecure registry
+echo '{"insecure-registries": ["sawtooth-registry:5000"]}' > /etc/docker/daemon.json
+# shellcheck disable=SC2046
+kill -SIGHUP $(pidof dockerd)
+
+# Wait for Docker to reload its configuration
+sleep 5
+
+# Execute the main command
+exec "$@"
