53f85ddaf0a263b0a44087de1ad4633c6c36cade
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-19 18:28:30 +1000

    Clean up the project and add structure.

--- deploy-network.sh
+++ deploy-network.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env bash
 
-# Script Name: deploy.sh
+# Script Name: deploy-network.sh
 # Description: Used to build all necessary images and start up a sawtooth network
 # Author: Murtaza Rangwala
 # Date Created: 2024-08-08
 # Last Modified: 2024-08-08
 
-# Usage: "./deploy.sh
+# Usage: "./deploy-network.sh
 
 # Main script content starts here
 
 WORK_DIR=$(pwd)
-TEST_APP_DIR=$(pwd)/../sample_application
+TEST_APP_DIR=$(pwd)/test-application
 
 # Make sure user is in the correct working directory
 cd "$WORK_DIR" || exit
+
+# Freeze dependencies
+pip freeze > fog-node/requirements.txt
+cp fog-node/requirements.txt auto-docker-deployment-family/docker-image-client/.
+cp fog-node/requirements.txt auto-docker-deployment-family/docker-image-tp/.
 
 # Bring docker compose down incase it is up
 docker-compose down -v --rmi all
@@ -27,7 +32,7 @@ docker build -t temp-anomaly-detection:latest -f Dockerfile .
 cd "$WORK_DIR" || exit
 
 # Export test docker application
-docker save -o docker-image-client/temp-anomaly-detection.tar temp-anomaly-detection
+docker save -o auto-docker-deployment-family/docker-image-client/temp-anomaly-detection.tar temp-anomaly-detection
 
 # Run docker compose
 docker-compose build
