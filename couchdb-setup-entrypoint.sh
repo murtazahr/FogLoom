46e65d7350fc6a070660ced6c306266d7cb477bb
Murtaza Hatim <murtazahatimr@icloud.com>
2024-08-29 13:50:14 +1000

    Add couchdb services to kubernetes deployment.

--- couchdb-setup-entrypoint.sh
+++ couchdb-setup-entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 # CouchDB cluster configuration
-PORT=5984
 DB_NAME="resource_registry"
 
 # Function to log messages
@@ -11,26 +10,33 @@ log_message() {
 
 log_message "Starting CouchDB cluster setup"
 
-sleep 5
+# Wait for all CouchDB instances to be ready
+for i in 0 1 2 3 4; do
+  until curl -s "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@sawtooth-$i:5984" > /dev/null; do
+    log_message "Waiting for CouchDB on sawtooth-$i to be ready..."
+    sleep 5
+  done
+  log_message "CouchDB on sawtooth-$i is ready"
+done
 
 log_message "Adding nodes to the cluster"
 for num in 1 2 3 4; do
-  response=$(curl -X POST -H 'Content-Type: application/json' "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb0.local:5984/_cluster_setup" -d "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \"username\": \"${COUCHDB_USER}\", \"password\":\"${COUCHDB_PASSWORD}\", \"port\": 5984, \"node_count\": \"5\", \"remote_node\": \"couchdb$num.local\", \"remote_current_user\": \"${COUCHDB_USER}\", \"remote_current_password\": \"${COUCHDB_PASSWORD}\" }")
-  log_message "Enable cluster on couch-db-$num response: ${response}"
-  response=$(curl -s -X POST -H 'Content-Type: application/json' "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb0.local:5984/_cluster_setup" -d "{\"action\": \"add_node\", \"host\":\"couchdb$num.local\", \"port\": 5984, \"username\": \"${COUCHDB_USER}\", \"password\":\"${COUCHDB_PASSWORD}\"}")
-  log_message "Adding node couch-db-$num response: ${response}"
+  response=$(curl -X POST -H 'Content-Type: application/json' "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@sawtooth-0:5984/_cluster_setup" -d "{\"action\": \"enable_cluster\", \"bind_address\":\"0.0.0.0\", \"username\": \"${COUCHDB_USER}\", \"password\":\"${COUCHDB_PASSWORD}\", \"port\": 5984, \"node_count\": \"5\", \"remote_node\": \"sawtooth-$num\", \"remote_current_user\": \"${COUCHDB_USER}\", \"remote_current_password\": \"${COUCHDB_PASSWORD}\" }")
+  log_message "Enable cluster on sawtooth-$num response: ${response}"
+  response=$(curl -s -X POST -H 'Content-Type: application/json' "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@sawtooth-0:5984/_cluster_setup" -d "{\"action\": \"add_node\", \"host\":\"sawtooth-$num\", \"port\": 5984, \"username\": \"${COUCHDB_USER}\", \"password\":\"${COUCHDB_PASSWORD}\"}")
+  log_message "Adding node sawtooth-$num response: ${response}"
 done
 
 log_message "Finishing cluster setup"
-response=$(curl -s -X POST -H 'Content-Type: application/json' "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb0.local:5984/_cluster_setup" -d "{\"action\": \"finish_cluster\"}")
+response=$(curl -s -X POST -H 'Content-Type: application/json' "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@sawtooth-0:5984/_cluster_setup" -d "{\"action\": \"finish_cluster\"}")
 log_message "Finish cluster response: ${response}"
 
 log_message "Checking cluster membership"
-membership=$(curl -s -X GET "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb0.local:5984/_membership")
+membership=$(curl -s -X GET "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@sawtooth-0:5984/_membership")
 log_message "Cluster membership: ${membership}"
 
 log_message "Creating ${DB_NAME} database on all nodes"
-response=$(curl -s -X PUT "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@couchdb0.local:5984/${DB_NAME}")
-log_message "Creating ${DB_NAME} on ${node} response: ${response}"
+response=$(curl -s -X PUT "http://${COUCHDB_USER}:${COUCHDB_PASSWORD}@sawtooth-0:5984/${DB_NAME}")
+log_message "Creating ${DB_NAME} on sawtooth-0 response: ${response}"
 
-sleep 2
+log_message "CouchDB cluster setup completed"
---
+++ deploy-k8s-network.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Main script content starts here
+
+WORK_DIR=$(pwd)
+TEST_APP_DIR=$(pwd)/example-application
+K8S_DIR=$(pwd)/k8s-manifests/sawtooth-network
+
+# Building docker image for test docker application
+cd "$TEST_APP_DIR" || exit
+docker build -t temp-anomaly-detection:latest -f Dockerfile .
+
+# Make sure user is in the correct working directory
+cd "$WORK_DIR" || exit
+
+# Export test docker application
+docker save -o auto-docker-deployment/docker-image-client/temp-anomaly-detection.tar temp-anomaly-detection
+
+# Build peer-registry-tp image
+docker build -t peer-registry-tp:local ./peer-registry/peer-registry-tp
+# Build docker-image-tp image
+docker build -t docker-image-tp:local ./auto-docker-deployment/docker-image-tp
+# Build docker-image-client image
+docker build -t docker-image-client:local ./auto-docker-deployment/docker-image-client
+# Build fog-node image
+docker build -t fog-node:local ./fog-node
+
+# Import images into k3s
+sudo k3s ctr images import peer-registry-tp:local
+sudo k3s ctr images import docker-image-tp:local
+sudo k3s ctr images import docker-image-client:local
+sudo k3s ctr images import fog-node:local
+
+echo "Images built and imported into k3s successfully"
+
+cd "$K8S_DIR" || exit
+
+# Cleanup kubernetes environment if it exists.
+sudo kubectl delete -f sawtooth-k8s-default-pbft.yaml
+
+# Bring up network
+sudo kubectl apply -f pbft-keys-configmap.yaml
+sudo kubectl apply -f sawtooth-k8s-default-pbft.yaml
