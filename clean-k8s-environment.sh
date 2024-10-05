03c744316d15b0f6a224e8ca01aa28c8172164a0
Murtaza Hatim <murtazahatimr@icloud.com>
2024-10-05 17:09:02 +1000

    Update clean script.

--- clean-k8s-environment.sh
+++ clean-k8s-environment.sh
@@ -6,7 +6,7 @@ kubectl delete -f kubernetes-manifests/generated/blockchain-network-deployment.y
 kubectl delete -f kubernetes-manifests/static/local-docker-registry-deployment.yaml
 kubectl delete -f kubernetes-manifests/generated/couchdb-cluster-deployment.yaml
 kubectl delete -f kubernetes-manifests/generated/config-and-secrets.yaml
-kubectl delete secret couchdb-secrets
+kubectl delete secret couchdb-certs
 kubectl delete secret redis-certificates
 kubectl delete secret redis-password
 kubectl delete pvc -l app=redis-cluster
